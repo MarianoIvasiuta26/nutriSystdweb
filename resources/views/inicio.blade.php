26975d2f35715e39c5e4d4c2f7a7fd804fd4af3b
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2023-06-29 15:59:57 -0300

    Instalación de AdminLTE y comienzo de personalización de la página principal

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pagina de Inicio</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-image: url('{{ asset('img/fondo-inicio.jpg') }}');
+            background-size: cover;
+            background-position: center;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+        }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        .button-container {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px; /* Ajuste para separar los botones del texto */
+        }
+
+        .button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column; /* Ajuste para colocar el texto debajo del botón */
+            width: 150px;
+            height: 150px;
+            border-radius: 50%;
+            background-color: rgba(255, 255, 255, 0.8);
+            margin: 0 10px;
+            cursor: pointer;
+            transition: transform 0.3s, box-shadow 0.3s;
+            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3);
+            position: relative;
+        }
+
+        .button:hover {
+            transform: scale(1.1);
+            box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.5);
+        }
+
+        .button img {
+            width: 60px;
+            height: 60px;
+        }
+
+        .button span {
+            margin-top: 10px; /* Ajuste para separar el texto del botón */
+            font-size: 18px; /* Ajuste para el tamaño del texto dentro de los botones */
+            color: rgba(0, 0, 0, 0.8);
+        }
+
+        .message {
+            position: absolute;
+            top: 30%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 50px; /* Ajuste para el tamaño del texto */
+            color: white;
+            text-align: center;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+        }
+        a {
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="message">NutriSoft - Sistema de Gestión Nutricional</div>
+   
+</div>
+</body>
+</html>
+
+
+
