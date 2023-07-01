e100984b7afba997c65b1868f93bbd53ccc836b7
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2023-06-30 22:08:13 -0300

    creé los roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 class RoleSeeder extends Seeder
 {
     /**
@@ -14,6 +17,11 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $role1 = Role::create(['name'=>'Administrador']);
+        $role2 = Role::create(['name' => 'Nutricionista']);
+        $role3 = Role::create(['name' => 'Paciente']);
+
+        $permission = Permission::create(['name' => 'solicitar turno']);
+        
     }
 }
